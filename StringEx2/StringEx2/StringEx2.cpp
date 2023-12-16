c10bb67d51e121bbf31d340b9e9ef2915b48475c
Proskura-Dima <149872164+Proskura-Dima@users.noreply.github.com>
2023-12-16 22:12:09 +0400

    Add files via upload

---
+++ StringEx2/StringEx2/StringEx2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string mains;
+    getline(cin, mains);
+    bool palindrom=false;
+    for (int i = 0; i < mains.length()/2; i++) {
+        if (mains.at(i) != mains.at(mains.length() - 1 - i)) {
+            palindrom = false;
+            break;
+        }
+        else {
+            palindrom = true;
+        }
+    }
+    if (palindrom) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    cout << endl;
+    for (int i = 0; i < mains.length(); i++) {
+        mains.replace(i, 1, 1, (char)toupper(mains[i]));
+    }
+    cout << mains;
+}
